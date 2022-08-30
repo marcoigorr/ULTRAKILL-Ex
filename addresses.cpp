61402cef4b3edf4e548d4047727eaf2707146aa4
marcoigorr <petronio.mrc@gmail.com>
2022-08-30 16:36:34 +0200

    New Offsets

--- addresses.cpp
+++ addresses.cpp
@@ -7,8 +7,15 @@
 
 void Address::calcAddresses()
 {
-	localPlayer = unityPlayer + 0x017AB300;
-	hp = mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->hp);
+	localPlayer =		unityPlayer + 0x017AB300;
+	walkSpeed =			mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->walkSpeed);
+	jumpPower =			mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->jumpPower);
+	airAcceleration =	mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->airAcceleration);
+	wallJumpPower =		mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->wallJumpPower);
+	timeBetweenSteps =	mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->timeBetweenSteps);
+	hp =				mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->hp);
+	walking =			mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->walking);
+	boostCharge =		mem->FindDMAAddyEx(proc->hProcess, localPlayer, offset->boostCharge);
 }
 
 Address* addr = new Address();
--- addresses.h
+++ addresses.h
@@ -7,7 +7,16 @@ public:
 	uintptr_t unityPlayer;
 	uintptr_t localPlayer;
 
+	// NewMovement
+	uintptr_t walkSpeed;
+	uintptr_t jumpPower;
+	uintptr_t airAcceleration;
+	uintptr_t wallJumpPower;
+	uintptr_t timeBetweenSteps;
 	uintptr_t hp;
+	uintptr_t walking;
+	uintptr_t boostCharge;
+
 
 	void calcAddresses();
 };
